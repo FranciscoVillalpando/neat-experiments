3e8c9e3040a24dcbe051fbf9f55bd6880afdd8d8
Francisco Villalpando <fdelgado@ooyala.com>
2019-07-04 12:03:54 -0500

    Improved extract_stats

--- extract_stats.py
+++ extract_stats.py
@@ -1,11 +1,29 @@
 
 import sys
-import glob
 import xlwt
 from parse import *
 from parse import compile
 from collections import defaultdict
 
+
+# key is What lines to search for 
+#   parse string
+#
+parseMap = {
+    "avg_fitness": {  # this name does not really matter
+        'searchString':'average fitness',  # String that will be used to search for the stat
+        'parser':compile("Population's average fitness: {avg_fitness:f} stdev: {stdev:f}") # String used to parse data in the searched line
+    },
+    "best_genome": {
+        'searchString':'Best fitness',
+        "parser": compile("Best fitness: {best_genome_fitness:f} - size: {best_genome_size} - species {best_genome_species:d} - id {best_genome_id:d}")
+    },
+    "gen_time": {
+        'searchString':'Generation time',
+        "parser": compile("Generation time: {gen_time:f} sec ({avg_time:f} average)"),
+        "alt_parser": compile("Generation time: {gen_time:f} sec") # Failback parser
+    } 
+}
 
 def getStatsDict(fileNameList, runsFirst = True):
     '''
@@ -32,33 +50,10 @@ def getStatsDict(fileNameList, runsFirst = True):
     '''
 
     # Dictionary organized in the following format : 
-    # 
-    # 
     #   [stat][run][gen]
     # where gen is an array
     # Example: statsDict['avg_fitness']['run2'][3]
     statsDict = defaultdict(lambda: defaultdict(list))
-
-    # key is What lines to search for 
-    #   parse string
-    #
-    parseMap = {
-        "avg_fitness": {  # this name does not really matter
-            'searchString':'average fitness',  # String that will be used to search for the stat
-            'parser':compile("Population's average fitness: {avg_fitness:f} stdev: {stdev:f}") # String used to parse data in the searched line
-        },
-        "best_genome": {
-            'searchString':'Best fitness',
-            "parser": compile("Best fitness: {best_genome_fitness:f} - size: {best_genome_size} - species {best_genome_species:d} - id {best_genome_id:d}")
-        },
-        "gen_time": {
-            'searchString':'Generation time',
-            "parser": compile("Generation time: {gen_time:f} sec ({avg_time:f} average)"),
-            "alt_parser": compile("Generation time: {gen_time:f} sec") # Failback parser
-        } 
-    }
-
-    #print(parseMap)
 
     for statFileName in fileNameList:
 
@@ -74,12 +69,15 @@ def getStatsDict(fileNameList, runsFirst = True):
                             parser = parseMap[keyString]['alt_parser']
                             parsedData = parser.parse(line)
                     
+                        clean_name = statFileName.split("/")[-1]
+
+                        #print(clean_name)
                         for statName, statValue in parsedData.named.items():
                             # Add result to dictionary
                             if runsFirst:
-                                statsDict[statFileName][statName].append(statValue)
+                                statsDict[clean_name][statName].append(statValue)
                             else:
-                                statsDict[statName][statFileName].append(statValue)
+                                statsDict[statName][clean_name].append(statValue)
 
                         
 
@@ -134,23 +132,67 @@ def generateXLS(filename,fileNameList):
                 #ws.write(rowIndex + 1, 0, rowIndex)
                 ws.write(rowIndex + 1, columnIndex + 1, populationVal)
 
+    wb.save(filename)
+    print("saved xls")
 
+def generateScoreTable(filename, fileNameList):
+    ''' Creates a table of state score vs run in a XLS:
+                r1  r2  r3
+        State1   X   X   X
+        State2   X   X   X
+    '''
+    statsDict = getStatsDict(fileNameList, False)
+     
+    #the code for creating the workbook and worksheets
+    wb= xlwt.Workbook()
+    ws = wb.add_sheet("Scores")
+
+    stateDict = defaultdict(lambda: defaultdict(float))
+    parser = compile("{:d}.txt")
+    # Generate state dict:
+    #   dict ["state"][randRun]
+    # note that since we dont care the order of the runs it is random in the list
+    # i.e. dict["state"][0] does not correspond to the first run
+    # ex:
+    #   dict["SpringYardZone.Act1.state"][0-4] = someScore
+    for logName in statsDict['best_genome_fitness']:
+
+        state = logName.split("_")[0]
+        run = parser.parse(logName.split("_")[2])[0]
+        print(state,run)
+        stateDict[state][run]  =  max(statsDict['best_genome_fitness'][logName])
+
+    print(stateDict)
+    # Write XLS based on the contents of the stateDict
+    ws.write(0, 0, "State")
+    #ws.write(0, 1, "Best fitness")
+    # Get run names
+    run_list = []
+    for idx, run_id in enumerate((list(stateDict.values())[0])):
+        run_list.append(run_id)
+        ws.write(0, idx+1, run_id)   
+
+    row = 1
+    for state in stateDict :
+        ws.write(row, 0, state)
+        for col, run_id in enumerate(run_list):
+            ws.write(row, col+1, stateDict[state][run_id])
+
+        row += 1
 
     wb.save(filename)
     print("saved xls")
 
+if __name__ == "__main__":
 
+    fileNameList = []
+    output_file_name = "test.xls"
+    # We are able to specify both a set of files or a glob
+    if len(sys.argv) > 3:
+        output_file_name = sys.argv[1]
+        fileNameList = sys.argv[2:]
+    else:
+        print("Need files to process")
+        exit
 
-
-fileNameList = []
-
-# If we don't provide the specific file names,
-# then search the current folder for runX.txt files
-if len(sys.argv) > 1:
-    fileNameList = sys.argv[1:]
-else:
-    fileNameList = glob.glob("*state_train_1560993029.txt")
-
-print(fileNameList)
-
-generateXLS("summary_1560993029.xls", fileNameList)
+    generateScoreTable(output_file_name, fileNameList)
